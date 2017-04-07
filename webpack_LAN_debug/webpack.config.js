6c3072c049d959c69775ad3a0c3652c7dcd0a134
Darcy_Wang <wangziangis@gmail.com>
2017-04-07 22:55:50 +0800

    init

---
+++ webpack_LAN_debug/webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  //入口文件
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8089',
+    './src/main.js'   
+    ],
+  //输出文件
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
